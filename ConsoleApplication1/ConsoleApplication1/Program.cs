7605312801c0c1554dc841f8c819dc042532346d
ktkakadiya <ktkakadiya@gmail.com>
2016-10-04 12:40:23 +0530

    Yield Keyword Program Implemented...

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var a in Returns())
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+        static IEnumerable<int> Returns()
+        {
+            yield return 1;
+            yield return 2;
+        }
+    }
+}
